e2884d21b7eee76958652371197d78dec91b8738
aesatchien <cjh123@yaoo.com>
2022-03-04 16:45:44 -0800

    toggles

--- robot/commands/auto_rotate_imu.py
+++ robot/commands/auto_rotate_imu.py
@@ -3,6 +3,10 @@ from math import copysign
 from wpilib import SmartDashboard
 from wpimath.controller import PIDController
 
+SmartDashboard.putNumber('/Imu/kf', 0)
+SmartDashboard.putNumber('/Imu/kp', 0)
+SmartDashboard.putNumber('/Imu/kd', 0)
+
 class AutoRotateImu(commands2.CommandBase):
     """
     Rotate the robot based a PID control with the heading coming from the drivetrain's IMU
@@ -14,16 +18,16 @@ class AutoRotateImu(commands2.CommandBase):
         self.container = container
         self.drive = drive
         self.degrees = degrees
-        self.controller = PIDController(.0000001, 0, 0.00000001)
-        self.feed_forward = 0.2
+        self.controller = PIDController(0.004, 0, 0.001)
+        self.feed_forward = 0.1
+        self.tolerance = 1.5
 
         self.addRequirements(drive)  # commandsv2 version of requirements
 
     def initialize(self) -> None:
         self.drive.arcade_drive(0, 0)
-        self.controller.setSetpoint(self.degrees)
-
         self.start_angle = self.drive.navx.getAngle()
+        self.controller.setSetpoint(self.degrees)
 
         """Called just before this Command runs the first time."""
         self.start_time = round(self.container.get_enabled_time(), 2)
@@ -40,7 +44,7 @@ class AutoRotateImu(commands2.CommandBase):
     def isFinished(self) -> bool:
         relative_orientation = self.drive.navx.getAngle() - self.start_angle
         
-        return abs(relative_orientation - self.degrees) < 2
+        return abs(relative_orientation - self.degrees) < self.tolerance
 
     def end(self, interrupted: bool) -> None:
         end_time = self.container.get_enabled_time()
