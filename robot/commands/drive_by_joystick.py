bcda89a447ba3c79302edd95e21dac255acfce98
aesatchien <cjh123@yaoo.com>
2022-03-07 23:22:43 -0800

    two sided acceleration

--- robot/commands/drive_by_joystick.py
+++ robot/commands/drive_by_joystick.py
@@ -16,13 +16,17 @@ class DriveByJoytick(commands2.CommandBase):  # change the name for your command
 
         self.max_thrust_velocity = constants.k_max_thrust_velocity  # 3 m/s
         self.max_twist_velocity = constants.k_max_twist_velocity  # 1.5 m/s
-        self.deadband = 0.08
+        self.deadband = 0.07
         self.multipliers = [1.0, 1.0, -1.0, -1.0]
 
         self.max_arcade_thrust = constants.k_thrust_scale
         self.max_arcade_twist = constants.k_twist_scale
         self.previous_thrust = 0
-        self.max_thrust_differential = 0.125  # 0.15 starts to get a bit tippy but not that bad
+
+        # since the robot back heavy, so the reverse needs to be stronger than the fwd - rev should be 0.125, fwd 0.25
+        self.max_thrust_differential = 0.07  # 0.05 starts to get a bit tippy but not that bad, cory likes 0.04
+        self.max_thrust_differential_fwd = 0.04
+        self.max_thrust_differential_rev = 0.08
 
     def initialize(self) -> None:
         """Called just before this Command runs the first time."""
@@ -35,23 +39,27 @@ class DriveByJoytick(commands2.CommandBase):  # change the name for your command
     def execute(self) -> None:
         # get thrust and twist from the controller
         thrust = -self.container.driver_controller.getRawAxis(constants.k_controller_thrust_axis)
-        thrust = 0 if abs(thrust) < self.deadband else thrust * self.max_thrust_velocity
+        thrust = 0 if abs(thrust) < self.deadband else thrust
         twist = self.container.driver_controller.getRawAxis(constants.k_controller_twist_axis)
-        twist = 0 if abs(twist) < self.deadband else twist * self.max_twist_velocity
+        twist = 0 if abs(twist) < self.deadband else twist
 
         # try to limit the change in thrust  BE VERY CAREFUL WITH THIS!  IT CAUSES RUNAWAY ROBOTS!
+        # limits are made using 3m/s speed limit - adjust if you change it
         limit_decel = True
         if limit_decel:
             d_thrust = self.previous_thrust - thrust
-            if abs(d_thrust) > self.max_thrust_differential:
-                thrust = self.previous_thrust - self.max_thrust_differential * math.copysign(1, d_thrust)
-                print(f'Applying decel limit: previous velocity: {self.previous_thrust:.3f} delta_velocity: {d_thrust:0.3f}')
+            max_thrust_differential = self.max_thrust_differential_rev if thrust < 0 else self.max_thrust_differential_fwd
+            if abs(d_thrust) > max_thrust_differential:
+                thrust = self.previous_thrust - max_thrust_differential * math.copysign(1, d_thrust)
+                print(f'Applying {1 * math.copysign(1, thrust)} limit: previous thrust: {self.previous_thrust:.3f} delta_thrust: {d_thrust:0.3f}')
         self.previous_thrust = thrust
 
         if self.control_type == 'velocity':
             self.drive.feed()
             # left front left back  right front  right back
-            velocities = [thrust + twist, thrust + twist, thrust - twist, thrust - twist]
+            thrust_vel = thrust * self.max_thrust_velocity
+            twist_vel = twist * self.max_twist_velocity
+            velocities = [thrust_vel + twist_vel, thrust_vel + twist_vel, thrust_vel - twist_vel, thrust_vel - twist_vel]
             if (thrust**2 + twist**2)**0.5 > 0.05:
                 for controller, velocity, multiplier in zip(self.drive.pid_controllers, velocities, self.multipliers):
                     controller.setReference(velocity * multiplier, rev.CANSparkMaxLowLevel.ControlType.kSmartVelocity, 1)
