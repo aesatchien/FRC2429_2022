ae359ecd5ceedeadf879f9065ddc8585d6fcecea
aesatchien <cjh123@yahoo.com>
2022-04-06 17:26:44 -0700

    leds

---
+++ robot/commands/led_loop.py
@@ -0,0 +1,31 @@
+import commands2
+from wpilib import SmartDashboard
+
+
+class LedLoop(commands2.CommandBase):
+
+    def __init__(self, container) -> None:
+        super().__init__()
+        self.setName('LedLoop')
+        self.container = container
+        self.addRequirements(container.robot_led)
+
+    def initialize(self) -> None:
+        """Called just before this Command runs the first time."""
+        self.start_time = round(self.container.get_enabled_time(), 2)
+        print("\n" + f"** Firing {self.getName()}  at {self.start_time} s **", flush=True)
+        SmartDashboard.putString("alert", f"** Started {self.getName()} at {self.start_time - self.container.get_enabled_time():2.2f} s **")
+
+    def execute(self) -> None:
+        pass
+
+    def isFinished(self) -> bool:
+        return False
+
+    def end(self, interrupted: bool) -> None:
+        end_time = self.container.get_enabled_time()
+        message = 'Interrupted' if interrupted else 'Ended'
+        print(f"** {message} {self.getName()} at {end_time:.1f} s after {end_time - self.start_time:.1f} s **", flush=True)
+        SmartDashboard.putString(f"alert", f"** {message} {self.getName()} at {end_time:.1f} s after {end_time - self.start_time:.1f} s **")
+
+
---
+++ robot/subsystems/led.py
@@ -0,0 +1,51 @@
+import wpilib
+from commands2 import SubsystemBase
+from wpilib import SmartDashboard, AddressableLED
+import constants
+
+
+class Led(SubsystemBase):
+    def __init__(self):
+        super().__init__()
+        self.setName('Led')
+        self.counter = 0
+        self.led_counter = 0
+
+        self.led_count = 20
+        self.led_strip = AddressableLED(constants.k_led_strip_port)
+        self.led_data = [AddressableLED.LEDData() for _ in range(self.led_count)]
+
+        self.led_strip.setLength(self.led_count)
+        self.led_strip.setData(self.led_data)
+        self.led_strip.start()
+        self.mode = 'static'
+        # self.LED_on = False
+
+    def periodic(self) -> None:
+        if self.counter % 10 == 0:
+            if self.mode == 'static':
+                [led.setRGB(255, 0, 0) for led in self.led_data]
+            elif self.mode == 'flash':
+                if self.led_counter % 2 == 0: # every other cycle
+                    [led.setRGB(0, 0, 255) for led in self.led_data]
+                else:
+                    [led.setRGB(0, 0, 0) for led in self.led_data]
+            elif self.mode == 'loading':
+                offset = self.led_counter % self.led_count
+                for led, i in enumerate(self.led_data):
+                    if offset <= i < offset + 5:
+                        led.setRGB(0, 255, 0)
+                    else:
+                        led.setRGB(0, 0, 0)
+
+            self.led_counter += 1
+
+        self.counter += 1
+
+
+
+
+
+
+
+
